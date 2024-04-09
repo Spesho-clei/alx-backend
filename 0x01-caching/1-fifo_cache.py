91340ce90e2d6645a1784a56f665b0fd62648292
Spesho-clei <jepkoechnancy56@gmail.com>
2024-04-09 17:07:07 +0000

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache inherits from BaseCaching and is a caching system using FIFO algorithm
+    """
+
+    def __init__(self):
+        """ Initialize the FIFO cache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            # Find the first item inserted (FIFO)
+            first_key = next(iter(self.cache_data))
+            print("DISCARD:", first_key)
+            del self.cache_data[first_key]
+            
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
